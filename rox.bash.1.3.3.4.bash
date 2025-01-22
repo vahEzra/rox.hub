bca1780d18181f49c35dad34615fc85d123ac736
vahEzra <lifecycle702@gmail.com>
2025-01-22 10:45:52 -0500

    Update rox.bash.1.3.3.4.bash

--- rox.bash.1.3.3.4.bash
+++ rox.bash.1.3.3.4.bash
@@ -67,32 +67,6 @@ fi
 echo "Rox Experience Download and Install Complete."
 sleep 2
 
-# Download Latest Roblox
-echo "Downloading Latest Roblox..."
-
-# Fetch Roblox version info
-robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
-robloxVersion=$(echo $robloxVersionInfo | jq -r ".clientVersionUpload")
-if [ -z "$robloxVersion" ]; then
-  echo "Failed to fetch Roblox version. Exiting."
-  exit 1
-fi
-
-# Download Roblox Player
-robloxZipPath="/tmp/RobloxPlayer.zip"
-curl -L "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "$robloxZipPath"
-echo "Download complete."
-sleep 2
-
-# Install Roblox
-echo "Installing Latest Roblox..."
-[ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
-unzip -o -q "$robloxZipPath" -d /tmp
-mv /tmp/RobloxPlayer.app "/Applications/Roblox.app"
-rm "$robloxZipPath"
-echo "Roblox installation complete."
-sleep 2
-
 # Final message
 echo "Finished Rox and Roblox installation."
 echo "Made by dexaru.net on Discord."
